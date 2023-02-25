df3dbf9b56b9d7b140534864a05782b31fe7cf9d
FerretCode <674313@salemkeizer.org>
2023-02-25 03:04:01 +0000

    finish

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # clay
-A Rust reverse proxy
+A Go reverse proxy
+
+## how it works
+- The proxy listens on port 3000
+- When a get request is received on the path `/`, it will look for a URL in the `target` header and try to proxy the request
+
+idk why i made this i was bored
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		target := r.Header.Get("target")
+
+		url, err := url.Parse(target)
+
+		if err != nil {
+			http.Error(w, "The target head did not contain a valid URL!", http.StatusBadRequest)
+
+			return
+		}
+
+		proxy := httputil.NewSingleHostReverseProxy(url)
+
+		proxy.Director = func(request *http.Request) {
+			request.Host = url.Host
+			request.URL.Scheme = url.Scheme
+			request.URL.Host = url.Host
+			request.URL.Path = url.Path
+		}
+
+		proxy.ServeHTTP(w, r)
+	})
+
+	http.ListenAndServe(":3000", r)
+}
